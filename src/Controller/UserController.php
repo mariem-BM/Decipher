f13f2297c3c73d3b1e2b924b45d82dcc16b09886
unknown <yacoubi.fatima@esprit.tn>
2022-02-14 20:45:13 +0100

    Read+Delete User Backend

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
 
 class UserController extends AbstractController
 {
@@ -16,5 +17,27 @@ class UserController extends AbstractController
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',
         ]);
+    }
+    /**
+     * @param UserRepository $repository
+     * @return /Symfony/Component/HttpFondation/Response;
+     * @Route("/AfficheUser",name="AfficheUser")
+     */
+    public function Affiche(UserRepository $repository)
+    {
+        $user=$repository->findAll();
+        return $this->render('user/AfficheUser.html.twig',['user'=>$user]);
+    }
+    /**
+     * @Route("/SupprimerUser/{id}",name="d")
+     */
+    function Delete($id,UserRepository $repository)
+    { $user=$repository->find($id);
+        $em=$this->getDoctrine()->getManager();
+        $em->remove($user);
+        $em->flush();
+        return $this->redirectToRoute('AfficheUser');
+
+
     }
 }
