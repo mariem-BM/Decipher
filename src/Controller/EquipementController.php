76779bff920e017e619ba187dcd2e7abb964abe0
unknown <yacoubi.fatima@esprit.tn>
2022-02-17 19:12:27 +0100

    .

---
+++ src/Controller/EquipementController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EquipementController extends AbstractController
+{
+    /**
+     * @Route("/equipement", name="equipement")
+     */
+    public function index(): Response
+    {
+        return $this->render('equipement/index.html.twig', [
+            'controller_name' => 'EquipementController',
+        ]);
+    }
+}
