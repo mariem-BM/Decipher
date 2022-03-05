8505d00bc8b090097afa2ea36fec02d64f76be5b
Maryem Ben Massaoud <maryem.benmassoud@esprit.tn>
2022-03-05 19:46:57 +0100

    Mailing-modif

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -2,6 +2,7 @@
 
 namespace App\Form;
 
+
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -14,7 +15,8 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('nom')
+            ->add('nom', TextareaType::class)
+            ->add('prenom', TextareaType::class)
             ->add('email', EmailType::class)
             ->add('message', TextareaType::class)
             ->add('envoyer', SubmitType::class)
